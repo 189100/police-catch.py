9a4c8208f6aa1aef28528271cc54e1c18e76e1c7
189100 <38845940+189100@users.noreply.github.com>
2018-08-03 21:13:14 +0530

    Add files via upload

---
+++ police catch.py
@@ -0,0 +1,15 @@
+'''Program to find the person is using the system legally or not'''
+
+#enter your age
+
+num = int (input("Enter your age:\n"))
+print (num)
+if num < 18:
+   print ("You are not using this system legally,\nPolice is on their way to catch you,\nYour game is now over, HaHaHa!!")
+if num > 18:
+   print ("You are using this system legally,\nCarry on your work, if you leave your work incompleted,\nI will call police, HaHaHa!!")
+if num == 18:
+   print ("You have nothing to do at this age.\nHaHaHa!!")
+   
+
+#program ends!!
